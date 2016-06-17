51d1965c08999dc1f5ca6873349e3ffd83fe9f60
Zhu Ran <zhuran94@163.com>
2016-06-17 22:37:17 +0800

    [Mengqiu] add event delegation example code.

--- a.js
+++ a.js
@@ -1,4 +1,16 @@
 
+function test(){
+	var father = document.getElementById('father');
+	var child = document.getElementById('child');
+	father.addEventListener('click', printOne, false);
+	child.addEventListener('click', printTwo, false);
+
+	// Event delegation
+	var table = document.getElementById('table');
+	console.log('table');
+	table.addEventListener('click', changeTdColor, false); //delegate changing td's color to its ancestor table
+}
+
 function StopBubble(e){
 	if(!e){
 		var e = window.event;
@@ -18,10 +30,11 @@ function printTwo(e){
 	StopBubble(e);
 }
 
-function test(){
-	var father = document.getElementById("father");
-	var child = document.getElementById("child");
-	father.addEventListener('click', printOne, false);
-	child.addEventListener('click', printTwo, false);
+function changeTdColor(e){
+	if(e.target.tagName.toLowerCase() == 'td'){
+		e.target.style.backgroundColor = 'yellow';
+	}
+
 }
+
 window.onload = test;
